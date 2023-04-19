d774c08719f1cc758c286bb8aadb657220adf64b
beomsangkim <131255086+beomsangkim@users.noreply.github.com>
2023-04-19 20:06:40 +1000

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,9 @@
+function showLoadingMessage() {
+    var loadingMessage = document.querySelector('.loading-message');
+    loadingMessage.style.display = 'block';
+    }
+  
+function hideLoadingMessage() {
+    var loadingMessage = document.querySelector('.loading-message');
+    loadingMessage.style.display = 'none';
+    }
