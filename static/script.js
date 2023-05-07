10bfdbd63108b34e98711547656dee65889aa49b
beomsangkim <131255086+beomsangkim@users.noreply.github.com>
2023-05-08 02:04:19 +1000

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,9 +1,9 @@
-function showLoadingMessage() {
-    var loadingMessage = document.querySelector('.loading-message');
-    loadingMessage.style.display = 'block';
+function showLoadingImage() {
+    var loadingImage = document.querySelector('.loading');
+    loadingImage.style.display = 'block';
     }
-  
-function hideLoadingMessage() {
-    var loadingMessage = document.querySelector('.loading-message');
-    loadingMessage.style.display = 'none';
+
+function hideLoadingImage() {
+    var loadingImage= document.querySelector('.loading');
+    loadingImage.style.display = 'none';
     }
